f8df120ce54fa171786f7c2a62bb24a5ae2a45f4
milind0110 <milindg0110@gmail.com>
2022-12-13 16:23:09 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,76 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpserver = createServer(app);
+const cors = require("cors");
+const ACTIONS = require("./src/Actions");
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+app.use(express.static("build"));
+
+httpserver.listen(PORT, () => {
+    console.log(`Listening to ${PORT} `)
+})
+
+const io = new Server(httpserver, {
+    cors: {
+        origin: "http://localhost:3000",
+        credentials: true,
+    },
+});
+
+
+const userSocketMap = {};
+
+
+async function getAllConnectedClients(roomId) {
+    const sockets = await io.in(roomId).fetchSockets();
+    return sockets.map((currSocket) => {
+        return { username: userSocketMap[currSocket.id], socketId: currSocket.id };
+    })
+}
+
+io.on('connection', (socket) => {
+    socket.on(ACTIONS.JOIN, async ({ roomId, username }) => {
+        socket.join(roomId);
+        userSocketMap[socket.id] = username;
+        const clients = await getAllConnectedClients(roomId);
+        io.to(roomId).emit(ACTIONS.JOINED, {
+            username,
+            socketId: socket.id,
+            clients
+        });
+    });
+
+    socket.on("disconnecting", async () => {
+        const [socketId, roomId] = Array.from(socket.rooms);
+        const clients = await getAllConnectedClients(roomId);
+        io.to(roomId).emit(ACTIONS.DISCONNECTED, {
+            username: userSocketMap[socketId],
+            socketId: socketId,
+            clients
+        })
+        delete userSocketMap[socket.id];
+    })
+
+    socket.on(ACTIONS.CODE_CHANGE,({code}) => {
+        const [socketId, roomId] = Array.from(socket.rooms);
+        socket.to(roomId).emit(ACTIONS.CODE_CHANGE, {
+            code
+        });
+    })
+
+    socket.on(ACTIONS.SYNC_CODE,({code, socketId}) => {
+        socket.to(socketId).emit(ACTIONS.CODE_CHANGE,{
+            code
+        })
+    })
+})
+
+
+
+
+
---
+++ src/pages/EditorPage.js
@@ -0,0 +1,91 @@
+import React, { useState, useRef, useEffect } from "react";
+import ACTIONS from "../Actions";
+import Client from "../components/Client"
+import Editor from "../components/Editor";
+import { useLocation, useParams } from "react-router-dom";
+import { io } from "socket.io-client";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+
+function showClient(Clientobj) {
+    return (
+        <Client key={Clientobj.socketId} username={Clientobj.username} />
+    );
+}
+
+function EditorPage() {
+    const navigate = useNavigate();
+    const { roomId } = useParams();
+    const [clients, setClients] = useState([]);
+    const socketRef = useRef(null);
+    const codeRef = useRef(null);
+    let location = useLocation();
+    useEffect(() => {
+        const init = () => {
+            socketRef.current = io(process.env.REACT_APP_BACKEND_URL);
+            socketRef.current.emit(ACTIONS.JOIN, {
+                roomId,
+                username: location.state.username,
+            })
+
+            //listening for joined event
+            socketRef.current.on(ACTIONS.JOINED, ({ username, socketId, clients }) => {
+                if (username !== location.state.username) {
+                    toast.success(`${username} joined the room`);
+                }
+                setClients([...clients]);
+                
+                socketRef.current.emit(ACTIONS.SYNC_CODE,{
+                    code : codeRef.current,
+                    socketId,
+                });
+            })
+
+            //listening for disconnected event
+            socketRef.current.on(ACTIONS.DISCONNECTED, ({ username : removedUsername, socketId : removedSocketId, clients : clients }) => {
+                console.log("removed");
+                toast.success(`${removedUsername} left the room`);
+                setClients(clients.filter(({ username, socketId }) => {
+                    return removedUsername !== username && removedSocketId !== socketId;
+                }));
+            })
+        };
+        init();
+    }, []);
+
+    
+    return (
+        <div className="mainWrapper">
+            <div className="sideBar">
+                <div className="sideBarUpper">
+                    <div className="sideBarSeprator">
+                        <img className="sideBarLogo" src="/images/whatsapp.png" alt=""></img>
+                    </div>
+                    <h3>Connected</h3>
+                    <div className="clientList">
+                        {clients.map(showClient)}
+                    </div>
+                </div>
+                <div className="sideBarLower">
+                    <button onClick={() => {
+                        navigator.clipboard.writeText(roomId);
+                        toast.success("Copied Room ID")
+                    }} className="btn copyBtn">Copy ROOM ID</button>
+                    <button onClick={() => {
+                        navigate('/');
+                    }} className="btn leaveBtn">Leave</button>
+                </div>
+            </div>
+            <div className="codeEditorWrap">
+                <Editor socketRef={socketRef} onCodeChange = {
+                    (code) => {
+                        codeRef.current = code;
+                    }
+                } />
+            </div>
+        </div>
+    );
+}
+
+export default EditorPage;
